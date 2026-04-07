d68c384a1c61dff0e96b18933eab2d9621e380c1
AnaRR8803 <arualana8803@gmail.com>
2026-04-06 21:19:01 -0600

    Lenjuages de programacion IV
    Actividad 1 Lenguajes de Programacion IV

---
+++ IMC_2026_Actividad1/src/imc_2026_actividad1/IMC_2026_Actividad1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package imc_2026_actividad1;
+
+/**
+ *
+ * @author rrlaa
+ */
+public class IMC_2026_Actividad1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ IMC_2026_Actividad1/src/imc_2026_actividad1/Persona.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package imc_2026_actividad1;
+
+/**
+ *
+ * @author rrlaa
+ */
+public class Persona {
+    
+    //Atributos
+    private float peso;
+    private float estatura;
+    private float imc;
+
+    //Propiedades
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    public float getImc() {
+        
+        return imc;
+    }
+
+    public void setImc() {
+        this.imc = this.peso / (this.estatura*this.estatura);
+    }
+    
+      
+}
